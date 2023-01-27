fe9460ffa621984825e85ffd7417781f2e8e36bf
RenLevingar <110576801+RenLevingar@users.noreply.github.com>
2023-01-26 17:06:52 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// on load
+function loader(){
+    alert("This is a secure browser do not share any information with other people.")
+}
+
+// on mouse move
+function wrong(){}
+
+// on click
+function submitter(){alert("Thank you for your info, please leave the site"); richard.disabled = true; richard.style.cursor = "no-drop";}
+
+// context menu
+function context(){document.addEventListener("contextmenu", (e) => {e.preventDefault()});}
+
+// on mouse enter and leave
+function noClick(){hubert.style.cursor = "none"; hubert.disabled = true;}
+function cursorReset(){hubert.style.cursor = "pointer"}
