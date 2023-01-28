3cac0880da61aa6d06bc80fbd850246a3427eff3
RenLevingar <110576801+RenLevingar@users.noreply.github.com>
2023-01-27 22:10:38 -0700

    Final
    Final Version of the project

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 // on load
-function loader(){
-    alert("This is a secure browser do not share any information with other people.")
-}
+function loader(){nixBTN.style.transition ="0s";alert("This is a secure browser do not share any information with other people.");}
 
 // on mouse move
 function correct(){richard.style.width = "200px";richard.style.height = "100px";richard.style.fontSize = "40px";}
 function correctReset(){richard.style.width = "100px";richard.style.height = "50px";richard.style.fontSize = "20px";}
 
 // on click
-function submitter(){alert("Thank you for your info, please leave the site"); richard.disabled = true; richard.style.cursor = "no-drop";}
+function submitter(){
+    alert("Thank you for your info, please leave the site");
+    richard.disabled = true;
+}
 
 // context menu
 function context(){document.addEventListener("contextmenu", (e) => {e.preventDefault()});}
 
 // on mouse enter and leave
 function noClick(){hubert.style.cursor = "none"; hubert.disabled = true;}
-function cursorReset(){hubert.style.cursor = "pointer"}
+function cursorReset(){hubert.style.cursor = "pointer";}
 
-// moving object
+// X and Y position
 page.onclick = function(event){
     let coords = this.getBoundingClientRect();
 
@@ -32,8 +33,23 @@ page.onclick = function(event){
 
 // for loop
 function remake(){
-   richard.style.color = "green";
-   for(let i=0; i>4; i++){
-    
-   }
-};
+    const buttons = document.getElementById('buttons');
+    for (let i = 0; i < 10; i++) {
+            const btn = document.createElement('button');
+            btn.style.background = "black";
+            btn.style.color = "white";
+            btn.style.fontSize = "15px";
+            btn.style.border = "1px solid black";
+            btn.style.width = "120px";
+            btn.innerHTML = "WRONG CHOICE"
+            buttons.appendChild(btn);
+        }
+    }
+
+// event listeners
+function buttonScale(){nixBTN.style.width = "60px";}
+let worker = document.createElement("button");
+worker.innerHTML = "Any suspicious activity wil be reported";
+testing.appendChild(worker);
+worker.addEventListener("click", tester);
+function tester(){worker.removeEventListener("click" , tester);console.log("event listener removed");};
